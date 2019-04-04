ff9512f451272d438f4093b9c8f73a9d91a76665
Ryan Campbell <c17407044@mydit.ie>
2019-04-04 11:45:19 +0100

    earth stats update

--- java/src/ie/tudublin/Edetails.java
+++ java/src/ie/tudublin/Edetails.java
@@ -1,5 +1,7 @@
 package ie.tudublin;
 
+import javax.security.auth.callback.LanguageCallback;
+
 public class Edetails
 {
     UI ui;
@@ -7,6 +9,17 @@ public class Edetails
     private float y;
     private float width;
     private float height;
+    private String species = "Species : Human";
+    private String planet = "Planet : MClass/Earth";
+    private String language = "Native language: English";
+    private String loyalty = "Loyalty : StarFleet";
+    private String threat = "Threat level: high";
+    private String population = "Population: 12.5billion";
+    private String capital = "Capital: Dublin";
+    private String surface = "Surface area: 1.96m miles";
+    private String image = "Earth suffered drastic lanscape \n reform after Co2 disaster";
+
+
     
 
     public Edetails(UI ui, float x, float y, float width, float height)
@@ -23,5 +36,16 @@ public class Edetails
     {
         ui.fill(58,134,183);
         ui.rect(x, y, width, height);
+
+        ui.fill(0,0,0);
+        ui.text(species, x+10, y+12);
+        ui.text(planet, x+10, y+26);
+        ui.text(threat, x+10, y+40);
+        ui.text(language, x+10, y+54);
+        ui.text(population, x+10, y+68);
+        ui.text(loyalty, x+10, y+82);
+        ui.text(capital, x+10, y+96);
+        ui.text(surface, x+10, y+110);
+        ui.text(image, x+10, y+126);
     }
 }
